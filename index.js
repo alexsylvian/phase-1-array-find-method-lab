57113f7671410289f6d29ae53544edbef6aa6776
Alexander <lazeratus1@gmail.com>
2023-06-18 16:08:41 -0400

    array iteration (tough)

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(records){
+    const found = records.find(element => element.result === `W`)
+    if (found) {
+        return found.year
+    }
+}
+
+
+/*function superbowlWin(element){
+    const found = element.find(element => element.found === `W`)
+    return found ? found.year : undefined
+}*/
+
+records.find(element => element.result === `W`)
